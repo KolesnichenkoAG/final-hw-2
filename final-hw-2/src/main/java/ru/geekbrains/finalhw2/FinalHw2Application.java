2348363a51862c7ba342ca7afabf3b11a15dc741
KolesnichenkoAG <kag.1981@mail.ru>
2023-01-24 23:41:52 +0600

    Initial commit

---
+++ final-hw-2/src/main/java/ru/geekbrains/finalhw2/FinalHw2Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.finalhw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalHw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalHw2Application.class, args);
+	}
+
+}
---
+++ final-hw-2/src/test/java/ru/geekbrains/finalhw2/FinalHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.finalhw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalHw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
